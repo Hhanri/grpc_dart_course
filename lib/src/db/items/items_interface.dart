007a98c864c5abab8aef127759aeaddea406be64
Hanri <henry018@laposte.net>
2022-05-28 16:53:23 +0200

    items/categories interfaces and implementations

---
+++ lib/src/db/items/items_interface.dart
@@ -0,0 +1,17 @@
+import 'package:dart_grpc_server/src/db/items/items_imp.dart';
+import 'package:dart_grpc_server/src/generated/groceries.pb.dart';
+
+abstract class InterfaceItemsServices {
+  factory InterfaceItemsServices() => ItemsServices();
+
+  Item? getItemByName(String name);
+  Item? getItemById(int id);
+  Item? createItem(Item item);
+  Item? editItem(Item item);
+  Empty? deleteItem(Item item);
+  List<Item>? getAllItems();
+  List<Item>? getItemsByCategory(int categoryId);
+
+}
+
+final itemsServices = InterfaceItemsServices();
