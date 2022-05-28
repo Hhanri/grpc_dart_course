7ff500e8becb41dc6e249e21a2b18a81e5d0c2b7
Hanri <henry018@laposte.net>
2022-05-28 17:57:17 +0200

    helper methods class

---
+++ lib/src/helper/helper.dart
@@ -0,0 +1,24 @@
+import 'package:dart_grpc_server/dart_grpc_server.dart';
+import 'package:dart_grpc_server/src/generated/groceries.pb.dart';
+
+class HelperMethods {
+  Category getCategoryFromMap(Map category) {
+    final idTag = 1;
+    final nameTag = 2;
+    int id = category['id'];
+    String name = category['name'];
+    return Category.fromJson('{"$idTag": $id, "$nameTag": "$name"}');
+  }
+
+  Item getItemFromMap(Map item) {
+    final idTag = 1;
+    final nameTag = 2;
+    final categoryTag = 3;
+    int id = item['id'];
+    String name = item['name'];
+    int categoryId = item['categoryId'];
+    return Item.fromJson('{"$idTag": $id, "$nameTag": $name. "$categoryId": $categoryId}');
+  }
+}
+
+final HelperMethods helper = HelperMethods();
